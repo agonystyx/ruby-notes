097dafee681756614ad7151b958550a936fca9fa
atrenholm <emptyleaking@gmail.com>
2016-01-17 22:28:40 -0500

    control structures

--- ruby-notes.mm
+++ ruby-notes.mm
@@ -49,7 +49,7 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="4"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
 <node TEXT="Recommended Readings" POSITION="right" ID="ID_1606566934" CREATED="1453063029430" MODIFIED="1453063033603">
 <edge COLOR="#ff0000"/>
 <node TEXT="Title Author Description&#xa;Eloquent Ruby Russ Olsen This book eloquently describes the Ruby programming language and does not put you to sleep in the process! Whether you are new to Ruby or have programmed in Ruby before, you will find &#x201c;Eloquent Ruby&#x201d; useful.&#xa;Programming Ruby Dave Thomas, Andy Hunt, and Chad Fowler The famous &#x201c;Pickaxe&#x201d; book, called so because of the pickaxe appearing on the cover. Complete Ruby reference.&#xa;Beginning Ruby Peter Cooper Excellent coverage for many Ruby topics.&#xa;Head First Ruby Jay McGavren This book just recently came out and everyone is talking about it! I am personally a huge fan of the Head First series - they explain stuff well and get you excited about learning in the process, so you don&apos;t fall asleep.&#xa;Learn To Program Chris Pine Great for Ruby beginners. Nice build up that won&apos;t overwhelm you. Some great examples of using Ruby in everyday life.&#xa;Ruby Cookbook Lucas Carlson, Leonard Richardson Lets you break your learning of Ruby down into chunks (recipes).&#xa;The Well-Grounded Rubyist David A. Black David Black knows his Ruby cold and is great at explaining it to others. The book has great reviews on Amazon.&#xa;Metaprogramming Ruby Paolo Perrotta A book about the cool &#x201c;don&#x2019;t-try-this-at-home&#x201d; Ruby features. :)" ID="ID_1116118875" CREATED="1453063071968" MODIFIED="1453063078897"/>
@@ -111,6 +111,63 @@
 <node TEXT="not exactly equal" ID="ID_104881361" CREATED="1453066293641" MODIFIED="1453066299026"/>
 <node TEXT="usually delegates to &quot;==&quot;" ID="ID_1637745970" CREATED="1453066299310" MODIFIED="1453066306403"/>
 <node TEXT="aka &quot;case equality operator&quot;" ID="ID_1689954443" CREATED="1453066380821" MODIFIED="1453066389710"/>
+</node>
+<node TEXT="Dynamic" POSITION="left" ID="ID_1837537038" CREATED="1453066851205" MODIFIED="1453066853799">
+<edge COLOR="#00ffff"/>
+</node>
+<node TEXT="Object-oriented" POSITION="left" ID="ID_799397876" CREATED="1453066854263" MODIFIED="1453066857744">
+<edge COLOR="#ffff00"/>
+<node TEXT="everything (almost) is an object" ID="ID_1915349976" CREATED="1453066858206" MODIFIED="1453066865218"/>
+</node>
+<node TEXT="3.times { puts &quot;Hello World!&quot;}" POSITION="right" ID="ID_1249913005" CREATED="1453066941537" MODIFIED="1453066970710">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="2 space indentation    suggested" POSITION="right" ID="ID_15832605" CREATED="1453066972917" MODIFIED="1453066979542">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="# comments" POSITION="right" ID="ID_1547368719" CREATED="1453066980178" MODIFIED="1453066986584">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="everything gets evaluated in Ruby" POSITION="right" ID="ID_497355889" CREATED="1453066997110" MODIFIED="1453067003794">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="output" POSITION="left" ID="ID_989458370" CREATED="1453067040479" MODIFIED="1453067042647">
+<edge COLOR="#ff0000"/>
+<node TEXT="puts" ID="ID_691390831" CREATED="1453067022069" MODIFIED="1453067046128">
+<node TEXT="put string" ID="ID_559134034" CREATED="1453067024387" MODIFIED="1453067026917"/>
+<node TEXT="returns nill" ID="ID_106615439" CREATED="1453067313835" MODIFIED="1453067317255"/>
+<node TEXT="side effect of printing out" ID="ID_625255937" CREATED="1453067317663" MODIFIED="1453067322862"/>
+</node>
+<node TEXT="p" ID="ID_1052981516" CREATED="1453067027990" MODIFIED="1453067046129">
+<node TEXT="prints out internal representation" ID="ID_731643428" CREATED="1453067030410" MODIFIED="1453067039016"/>
+</node>
+</node>
+<node TEXT="irb" POSITION="left" ID="ID_980695888" CREATED="1453067264492" MODIFIED="1453067286935">
+<edge COLOR="#ff00ff"/>
+<font BOLD="true"/>
+<node TEXT="interactive ruby" ID="ID_1746270136" CREATED="1453067266416" MODIFIED="1453067271488">
+<node TEXT="REPL" ID="ID_795800008" CREATED="1453067271940" MODIFIED="1453067273704"/>
+</node>
+</node>
+<node TEXT="conventions" POSITION="left" ID="ID_371016022" CREATED="1453067290631" MODIFIED="1453067293876">
+<edge COLOR="#00ffff"/>
+<node TEXT="naming conventions" ID="ID_208949297" CREATED="1453067126907" MODIFIED="1453067301483">
+<node TEXT="variables" ID="ID_1398364671" CREATED="1453067155590" MODIFIED="1453067158120">
+<node TEXT="lowercase" ID="ID_1674691745" CREATED="1453067140230" MODIFIED="1453067143062"/>
+<node TEXT="snake_case" ID="ID_773763742" CREATED="1453067143575" MODIFIED="1453067147646"/>
+</node>
+<node TEXT="constants" ID="ID_1830193851" CREATED="1453067158469" MODIFIED="1453067162472">
+<node TEXT="ALL_CAPS" ID="ID_532008749" CREATED="1453067168082" MODIFIED="1453067175074"/>
+<node TEXT="FirstCap" ID="ID_949906374" CREATED="1453067175451" MODIFIED="1453067188484"/>
+</node>
+<node TEXT="Classes (and Modules)" ID="ID_884353534" CREATED="1453067201053" MODIFIED="1453067205950">
+<node TEXT="CamelCase" ID="ID_1539161524" CREATED="1453067206463" MODIFIED="1453067212415"/>
+</node>
+</node>
+<node TEXT="semicolons" ID="ID_954646210" CREATED="1453067243412" MODIFIED="1453067306152">
+<node TEXT="leave off" ID="ID_1081344068" CREATED="1453067246257" MODIFIED="1453067248629"/>
+<node TEXT="can be used to separate statements on same line" ID="ID_1363455146" CREATED="1453067249085" MODIFIED="1453067261366"/>
+</node>
 </node>
 </node>
 </map>
